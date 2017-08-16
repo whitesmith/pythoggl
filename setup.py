d7740a23bd1363350e6c1f3f68ee34aff93270c1
Nuno Correia <nunolopesrb@gmail.com>
2017-08-16 18:11:32 +0100

    add basic setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythoggl',
+      version='0.1',
+      description='Just a simple wrapper for toggl',
+      url='https://github.com/whitesmith/pythoggl',
+      author='Nuno Lopes',
+      author_email='nunolopesrb@gmail.com',
+      license='MIT',
+      packages=['pythoggl'],
+      zip_safe=False)
